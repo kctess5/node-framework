9d0a7cf19bc549b31df439b7b231e3886d662931
Corey Walsh <corey@pifany.net>
2013-10-29 00:42:32 -0400

    First Commit

---
+++ config.rb
@@ -0,0 +1,32 @@
+# http://compass-style.org/help/tutorials/configuration-reference/
+
+require 'zurb-foundation'
+require 'animation'
+#add_import_path "/Library/Ruby/Gems/1.8/gems/animation-0.1.alpha.3/stylesheets"
+
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+http_path = "public"
+css_dir = "public/stylesheets"
+sass_dir = "public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+ line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
---
+++ public/javascripts/global.js
@@ -0,0 +1,7 @@
+console.log("global.js...");
+
+$(function() {
+
+	var socket = io.connect(); 
+
+});
